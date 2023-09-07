b3bf8e2f7489e7ac04213784d719af52d36af2da
mmdreza <m13r81a@gmail.come>
2023-09-07 19:27:44 +0330

    End of Header Section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,27 @@
 /*=============== SHOW MENU ===============*/
-
+const navToggle = document.getElementById('nav-toggle')
+const navClose = document.getElementById('nav-close')
+const navMenu = document.getElementById('nav-menu')
 
 /*=============== REMOVE MENU MOBILE ===============*/
-
+if(navToggle){
+    navToggle.addEventListener("click",()=>{
+        navMenu.classList.add('show-menu')
+    })
+}
 
 /*=============== GSAP ANIMATION ===============*/
+if(navClose){
+    navClose.addEventListener("click",()=>{
+        navMenu.classList.remove("show-menu")
+    })
+}
 
+
+// ========Remove Menu Mobile ================
+const navLink = document.querySelectorAll(".nav__link")
+const linkAction=()=>{
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(link =>link.addEventListener("click", linkAction))
