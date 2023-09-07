1cae46b39142ec8ccc0ab81a6994acfce91a5faa
mmdreza <m13r81a@gmail.come>
2023-09-07 20:53:32 +0330

    responsiv and animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,7 @@ if(navToggle){
     })
 }
 
-/*=============== GSAP ANIMATION ===============*/
+
 if(navClose){
     navClose.addEventListener("click",()=>{
         navMenu.classList.remove("show-menu")
@@ -25,3 +25,15 @@ const linkAction=()=>{
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(link =>link.addEventListener("click", linkAction))
+
+
+
+/*=============== GSAP ANIMATION ===============*/
+gsap.from(".home__points", 1.5 , {opacity:0,y:-300, delay:.2})
+gsap.from(".home__rocket", 1.5 , {opacity:0,y:300, delay:.3})
+gsap.from(".home__planet-1", 1.5 , {opacity:0,x:-200, delay:.8})
+gsap.from(".home__planet-2", 1.5 , {opacity:0,x:200, delay:1})
+gsap.from(".home__cloud-1", 1.5 , {opacity:0,y:200, delay:1.2})
+gsap.from(".home__cloud-2", 1.5 , {opacity:0,x:200, delay:1.3})
+gsap.from(".home__content", 1.5 , {opacity:0,y:-100, delay:1.4})
+gsap.from(".home__title img", 1.5 , {opacity:0,x:100, delay:1.6})
